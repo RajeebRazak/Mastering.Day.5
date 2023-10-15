fb6688ee7940511b52ebe7d2df3fb9b098fb39f0
RajeebRazak <145429667+RajeebRazak@users.noreply.github.com>
2023-10-15 23:03:00 +0530

    Add files via upload

---
+++ pratice.js
@@ -0,0 +1,52 @@
+var a = 10;
+var  b = a--;
+console.log(b);
+console.log(a)
+
+function sumsung(){
+    console.log("this is sumsung")
+}
+sumsung()
+
+function iphone(){
+    console.log("this is iphone")
+}
+iphone()
+
+function redmi(){
+    console.log("this is redmi")
+}
+redmi()
+
+var a = 10;
+var b = 20;
+
+function add(){
+    console.log(a+b)
+}
+add()
+
+var factor = "kamal";
+var fpalyer = "sachin";
+var fmovie = "anbe sivam"
+
+function favtor(){
+    console.log(factor+fmovie)
+}
+favtor()
+
+var factor = " kamal";
+var fpalyer = " sachin";
+var fmovie = " anbe sivam";
+
+function favtor(){
+    console.log("favrate actor is" + factor);
+    console.log("favrate actor is" + fpalyer);
+    console.log("favrate movie is" + fmovie);
+}
+favtor()
+
+function area(length,breath){
+    console.log(length*breath)
+}
+area(5,9)
